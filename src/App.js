a1b87bce77f3becb5ae62911f88f309fba4dbf23
nelo <nelokwaz@gmail.com>
2020-12-31 19:26:15 +0100

    moved the nav section to the Header component

--- src/App.js
+++ src/App.js
@@ -6,33 +6,17 @@ import {
   Route,
   NavLink,
 } from "react-router-dom";
-import Tab from "./components/Tab";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Features from "./pages/Features";
+import Header from "./components/Header";
 
 function App() {
   return (
     <Router>
       <div className="app">
         <div className="browser">
-          <div className="tabs">
-            <Tab>
-              <NavLink to="/" activeClassName="is-active" exact={true}>
-                Home
-              </NavLink>
-            </Tab>
-            <Tab>
-              <NavLink to="/about" activeClassName="is-active">
-                About
-              </NavLink>
-            </Tab>
-            <Tab>
-              <NavLink to="/features" activeClassName="is-active">
-                Features
-              </NavLink>
-            </Tab>
-          </div>
+          <Header />
 
           <div className="viewport">
             {/*Here we did put the route where we want the component to be displayed*/}
---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Tab from "./components/Tab";
+
+export default function Header() {
+  return (
+    <div className="tabs">
+      <Tab>
+        <NavLink to="/" activeClassName="is-active" exact={true}>
+          Home
+        </NavLink>
+      </Tab>
+      <Tab>
+        <NavLink to="/about" activeClassName="is-active">
+          About
+        </NavLink>
+      </Tab>
+      <Tab>
+        <NavLink to="/features" activeClassName="is-active">
+          Features
+        </NavLink>
+      </Tab>
+    </div>
+  );
+}
