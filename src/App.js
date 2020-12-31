8c8b1cff45a27cd73ef0faad9a1d51defc340e6f
nelo <nelokwaz@gmail.com>
2020-12-31 18:27:18 +0100

    created a separate page for the home page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React, { useState } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Tab from "./components/Tab";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="app">
-      <div className="browser">
-        <div className="tabs">
-          <Tab>
-            <a>Home</a>
-          </Tab>
-          <Tab>
-            <a>About</a>
-          </Tab>
-          <Tab>
-            <a>Features</a>
-          </Tab>
-        </div>
+    <Router>
+      <div className="app">
+        <div className="browser">
+          <div className="tabs">
+            <Tab>
+              <a>Home</a>
+            </Tab>
+            <Tab>
+              <a>About</a>
+            </Tab>
+            <Tab>
+              <a>Features</a>
+            </Tab>
+          </div>
 
-        <div className="viewport">Pages Go Here</div>
+          <div className="viewport">Pages Go Here</div>
+        </div>
       </div>
-    </div>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Router>
   );
 }
 
