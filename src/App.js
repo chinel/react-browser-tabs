27467e8c0bedb39198c259438b6ff715500b4581
nelo <nelokwaz@gmail.com>
2020-12-30 17:11:33 -1200

    created a function to handle on mouse event on the browser tabs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [highlightStyle, setHighlightStyle] = useState({ left: 0 });
+
+  function moveHighlight(event) {
+    //update highlight style to move the highlight
+  }
   return (
     <div className="app">
       <div className="browser">
         <div className="tabs">
-          <div className="tab">
-            <div className="highlight" />
+          <div className="tab" onMouseMove={moveHighlight}>
+            <div className="highlight" style={highlightStyle} />
             <a>Home</a>
           </div>
           <div className="tab">
